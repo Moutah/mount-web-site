d88c9b75bf0d8d48e28629cbdfe4ae527a99cd35
Moutah <mathieu.tappolet@gmail.com>
2020-11-23 18:51:30 +0100

    Added email injection

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+(() => {
+  console.log("hello world");
+
+  function substituteEmail() {
+    const placeholder = document.getElementById("mail-placeholder");
+    placeholder.innerHTML = `<a href="mailto:hello@mountweb.ch">hello@mountweb.ch</a>`;
+  }
+
+  window.onload = substituteEmail;
+})();
